273b47739bde58223a3c77d2bd537f93e3004317
Vinnix Schmidt <vinnix.bsd@gmail.com>
2012-05-21 09:57:03 -0300

    first commit of java tests with jdbc

---
+++ xxQueueSizeUtilities.java
@@ -0,0 +1,57 @@
+//package xxspiralutilities;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class xxQueueSizeUtilities {
+    public static String getQueueSize() {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException cnfe) {
+            System.err.println("Couldn't find driver class:");
+            System.out.println("Couldn't find driver class:");
+            cnfe.printStackTrace();
+        }
+        System.out.println("Able to locate the jmake a connection.");
+        Connection postGresConn = null;
+        try {
+            postGresConn =
+                    DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "pgsql", "pgsql");
+        } catch (SQLException se) {
+            System.out.println("Couldn't connect: print out a stack trace and exit.");
+            se.printStackTrace();
+            System.exit(1);
+        }
+
+        if (postGresConn != null)
+            System.out.println("Successfully connected to Postgres Database");
+        else
+            System.out.println("We should never get here.");
+
+        try {
+            Statement stGetCount = postGresConn.createStatement();
+            ResultSet rs =
+                //stGetCount.executeQuery("SELECT SUM(import_count -import_remaining) from  xx_queue_table where finish is null and import_end is null");
+                stGetCount.executeQuery("SELECT * from tb_teste;");
+            //ResultSet rs = stGetCount.executeQuery("SELECT SUM(import_count -import_remaining) from  xx_queue_table") ;
+            while(rs.next())
+	    {
+		System.out.println(rs.getString(1));
+	    }
+            return "OK";
+        } catch (SQLException e) {
+            System.out.println("Could not create statement in JDBC");
+            e.printStackTrace();
+
+        }
+        return "SUCCESS";
+    }
+
+    public static void main(String[] args) {
+        String textString = getQueueSize();
+        System.out.println("Result is " + textString);
+    }
+}
+
