ef75641122d03c3e1385f86e053dbf58eee7f89d
Vinnix Schmidt <vinnix.bsd@gmail.com>
2012-05-21 10:33:12 -0300

    some ident fixes

--- JDBCExample.java
+++ JDBCExample.java
@@ -2,48 +2,46 @@ import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.SQLException;
  
-public class JDBCExample {
- 
-	public static void main(String[] argv) {
+public class JDBCExample 
+{
+	public static void main(String[] argv) 
+	{
  
 		System.out.println("-------- PostgreSQL "
 				+ "JDBC Connection Testing ------------");
  
-		try {
- 
+		try 
+		{
 			Class.forName("org.postgresql.Driver");
- 
-		} catch (ClassNotFoundException e) {
- 
+		} 
+		catch (ClassNotFoundException e) 
+		{
 			System.out.println("Where is your PostgreSQL JDBC Driver? "
 					+ "Include in your library path!");
 			e.printStackTrace();
 			return;
- 
 		}
  
 		System.out.println("PostgreSQL JDBC Driver Registered!");
- 
 		Connection connection = null;
- 
-		try {
- 
-			connection = DriverManager.getConnection(
-					"jdbc:postgresql://127.0.0.1:5432/postgres", "pgsql", "pgsql");
- 
-		} catch (SQLException e) {
- 
+		try 
+		{
+			connection = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/postgres", "pgsql", "pgsql");
+		} 
+		catch (SQLException e) 
+		{
 			System.out.println("Connection Failed! Check output console");
 			e.printStackTrace();
 			return;
- 
 		}
  
-		if (connection != null) {
+		if (connection != null) 
+		{
 			System.out.println("You made it, take control your database now!");
-		} else {
+		} 
+		else 
+		{
 			System.out.println("Failed to make connection!");
 		}
 	}
- 
 }
